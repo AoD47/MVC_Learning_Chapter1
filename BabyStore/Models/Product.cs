fa34bec844ef06fd9c7e2f8106e37ac2150e61cf
Andre Kroukamp <Ajkroukamp@gmail.com>
2017-11-20 16:50:44 +0200

    Update

---
+++ BabyStore/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace BabyStore.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Descrioption { get; set; }
+        public decimal Price { get; set; }
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
