53b312bfb0631fdf52c46aff68bd09ffadef7019
Andre Kroukamp <Ajkroukamp@gmail.com>
2017-11-23 17:14:44 +0200

    Update 11/23 Work

---
+++ BabyStore/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BabyStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BabyStore.ViewModels
+{
+    public class ProductIndexViewModel
+    {
+        public IQueryable<Product> Products { get; set; }
+        public string Search { get; set; }
+        
+    }
+}
