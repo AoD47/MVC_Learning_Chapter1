ac9f622e171aa479f27868260ef0a3a3386291d9
Andre <AndreKroukamp140791@gmail.com>
2017-11-23 21:49:02 +0200

    Update 11/23 Home

--- BabyStore/Controllers/ProductsController.cs
+++ BabyStore/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
+using BabyStore.ViewModels;
+using System.Collections.Generic;
 
 namespace BabyStore.Controllers
 {
@@ -14,27 +16,51 @@ namespace BabyStore.Controllers
         private StoreContext db = new StoreContext();
 
         // GET: Products
-        public ActionResult Index(string category, string search)
+        public ActionResult Index(string category, string search, string sortBy)
         {
-            IQueryable<Product> products = db.Products.Include(p => p.Category);
-
+            //instantiate a new view model
+            ProductIndexViewModel viewModel = new ProductIndexViewModel();
+            //select the products
+            var products = db.Products.Include(p => p.Category);
+            //perform the search and save the search string to the viewModel
             if (!String.IsNullOrEmpty(search))
             {
                 products = products.Where(p => p.ProductName.Contains(search) ||
                 p.Description.Contains(search) ||
                 p.Category.CategoryName.Contains(search));
-                ViewBag.Search = search;
+                viewModel.Search = search;
             }
-
+            //group search results into categories and count how many items in each category
+            viewModel.CatsWithCount = from matchingProducts in products
+                                      where
+                                      matchingProducts.CategoryID != null
+                                      group matchingProducts by
+                                      matchingProducts.Category.CategoryName into
+                                      catGroup
+                                      select new CategoryWithCount()
+                                      {
+                                          CategoryName = catGroup.Key,
+                                          ProductCount = catGroup.Count()
+                                      };
             if (!String.IsNullOrEmpty(category))
             {
                 products = products.Where(p => p.Category.CategoryName == category);
             }
-
-            IQueryable<string> categories = products.OrderBy(p => p.Category.CategoryName).Select(p => p.Category.CategoryName).Distinct();
-            ViewBag.Category = new SelectList(categories);
-
-            return View(products.ToList());
+            //sort the results
+            switch (sortBy)
+            {
+                case "price_lowest":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                case "price_highest":
+                    products = products.OrderByDescending(p => p.Price);
+                    break;
+                default:
+                    break;
+            }
+            viewModel.Products = products;
+            viewModel.Sorts = new Dictionary<string, string> { { "Price low to high", "price_lowest" }, { "Price high to low", "price_highest" } };
+            return View(viewModel);
         }
 
         // GET: Products/Details/5
