5e4395fd3f7abcf323ec9655af53eb704d5b1ac7
Andre Kroukamp <Ajkroukamp@gmail.com>
2017-11-28 16:14:14 +0200

    Update 11/28 Work

--- BabyStore/App_Start/RouteConfig.cs
+++ BabyStore/App_Start/RouteConfig.cs
@@ -14,6 +14,30 @@ namespace BabyStore
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ProductsCreate",
+                url: "Products/Create",
+                defaults: new { controller = "Products", action = "Create" }
+            );
+
+            routes.MapRoute(
+                name: "CategoriesCreate",
+                url: "Categories/Create",
+                defaults: new { controller = "Categories", action = "Create" }
+            );
+
+            routes.MapRoute(
+                name: "ProductsIndex",
+                url: "Products",
+                defaults: new { controller = "Products", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "CategoriesIndex",
+                url: "Categories",
+                defaults: new { controller = "Categories", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
